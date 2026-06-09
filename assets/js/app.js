e8a47b31921602972fa4891f13184725cb9238f8
Leslie Figueroa <lesliek.x1999@gmail.com>
2026-06-09 18:14:05 -0400

    crear clase AdministradorUsuarios con constructor XHR

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+class AdministradorUsuarios {
+    constructor(url) {
+        this.url = url;
+        this.usuarios =[];
+
+        //XHR síncrono: false = espera hasta que llegue respuesta
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", this.url, false);
+        xhr.send;
+
+        if (xhr.status === 200){
+            this.usuarios = JSON.parse(xhr.responseText);
+            console.log("Data cargada: ", this.usuarios);
+        }
+    }
+}
